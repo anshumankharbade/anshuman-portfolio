046d5daf4857d6be165d59abc1249f60e6a59e27
Anshuman Sohan Kharbade <anshumankharbade28@gmail.com>
2025-12-14 14:34:38 +0530

    Add Rollup visualizer plugin for bundle analysis

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { visualizer } from 'rollup-plugin-visualizer';
 
-// https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [react(), tailwindcss()]
+  plugins: [tailwindcss(), react(), 
+    visualizer({
+      filename: 'bundle-analysis.html',
+      open: true,
+      gzipSize: true,
+      brotliSize: true,
+    })
+  ],
 })
